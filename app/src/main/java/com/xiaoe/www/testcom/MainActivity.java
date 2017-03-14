d9ff302ad65830f643bc24cfe8356f03e92ffa4c
tiangou <18827625270@163.com>
2017-03-14 21:05:18 +0800

    添加一个按钮

--- app/src/main/java/com/xiaoe/www/testcom/MainActivity.java
+++ app/src/main/java/com/xiaoe/www/testcom/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        mButton = (Button) findViewById(R.id.bt);
+        mButton.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +53,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.v("dd","这是真的吗");
     }
 }
